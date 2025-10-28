43e740daa72b22372740dd5d1c2ff699e02b0d75
AnaisDjenadi <151012902+AnaisDjenadi@users.noreply.github.com>
2025-10-28 19:12:02 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+const artistes = [
+  {
+    title: "Édith Piaf",
+    desc: "Édith Piaf, nicknamed 'La Môme', was one of France’s greatest singers. She is famous worldwide for her timeless songs such as 'La Vie en Rose' and 'Non, je ne regrette rien'.",
+    img: "images/edith_piaf.jpg"
+  },
+  {
+    title: "Johnny Hallyday",
+    desc: "Johnny Hallyday, known as 'the idol of the young', was one of the most iconic French rock singers. His career spanned more than 50 years, leaving a lasting mark on French music.",
+    img: "images/johnhy_hallyday.jpg"
+  },
+  {
+    title: "Claude François",
+    desc: "Claude François was a charismatic French pop singer from the 70s, best known for his energetic performances and hits such as 'Alexandrie Alexandra' and 'Comme d’habitude' (the original 'My Way').",
+    img: "images/claude_francois.jpg"
+  },
+  {
+    title: "Françoise Hardy",
+    desc: "Françoise Hardy is an icon of the 1960s — a French singer-songwriter with a soft, melancholic voice. Her poetic songs and elegant style influenced generations of artists.",
+    img: "images/francois_hardy.jpg"
+  },
+  {
+    title: "Mylène Farmer",
+    desc: "Mylène Farmer is a modern French pop icon known for her mysterious personality, poetic lyrics, and visually stunning live performances.",
+    img: "images/mylene_farmer.jpg"
+  }
+];
